d1ef5b07c51ca3c7ee81cd0df98d286b41a09a77
ting850911 <ytwang4@myseneca.ca>
2024-10-25 13:14:08 -0400

    fix gulp to compile scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,11 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function compileSass() {
-  return src('./src/sass/*.sass')
-    .pipe(sass())
-    .pipe(dest('./src/dist'));
+  return src('./src/sass/*.{sass,scss}').pipe(sass().on('error', sass.logError)).pipe(dest('./src/dist'));
 }
 
 function watchSass() {
-  watch('./src/sass/*.sass', compileSass);
+  watch('./src/sass/*.{sass,scss}', compileSass);
 }
 
 exports.default = series(compileSass, watchSass);
