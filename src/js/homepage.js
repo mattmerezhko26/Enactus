88d443cde9f157d77a8f92f7e3735bf9b1ac6913
ting850911 <ytwang4@myseneca.ca>
2024-11-13 20:11:23 -0500

    add rwd all pages

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -1,20 +1,26 @@
-const swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 15,
-  // scrollbar: {
-  //     el: ".swiper-scrollbar",
-  //     hide: false,
-  // },
-  autoplay: {
+document.addEventListener('DOMContentLoaded', () => {
+  // Swiper
+  const swiper = new Swiper('.mySwiper', {
+    slidesPerView: 1,
+    spaceBetween: 15,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
       delay: 2000,
       disableOnInteraction: false,
-  },
-  // pagination: {
-  //     el: ".swiper-pagination",
-  //     clickable: true,
-  // },
-  navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-  },
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      992: {
+        slidesPerView: 4,
+      },
+    },
+  });
 });
