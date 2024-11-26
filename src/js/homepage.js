965eb27ccd3d2a8e9bb2c92830d936b05d2f379c
mattmerezhko26 <merezhkomatt@gmail.com>
2024-11-26 09:41:36 -0500

    Fixing the deoartment section as well as js

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -1,20 +1,23 @@
-const swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 15,
-  // scrollbar: {
-  //     el: ".swiper-scrollbar",
-  //     hide: false,
-  // },
-  autoplay: {
+let swiper;
+
+function initializeSwiper() {
+  
+  if (swiper) {
+    swiper.destroy(true, true);
+  }
+  swiper = new Swiper(".mySwiper", {
+    slidesPerView: window.innerWidth < 768 ? 2 : 4, 
+    spaceBetween: 15,
+    autoplay: window.innerWidth >= 768 ? {
       delay: 2000,
       disableOnInteraction: false,
-  },
-  // pagination: {
-  //     el: ".swiper-pagination",
-  //     clickable: true,
-  // },
-  navigation: {
+    } : false,
+    navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
-  },
-});
+    },
+  });
+}
+
+initializeSwiper();
+window.addEventListener('resize', initializeSwiper);
